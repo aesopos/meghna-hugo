dd1828e0b40210fa514a40fcd70e113c289ee7a4
jhbrunoKim <bobhappy2000@gmail.com>
2020-03-06 17:20:52 +0900

    비디오 테스트

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,7 @@ jQuery(function ($) {
 
 
 	/* ========================================================================= */
-	/*	Magnific popup
+	/*	Magnific popup image
 	/* =========================================================================  */
 	$('.image-popup').magnificPopup({
 		type: 'image',
@@ -34,6 +34,30 @@ jQuery(function ($) {
 		fixedContentPos: false,
 		fixedBgPos: true
 	});
+	/* ========================================================================= */
+	/*	Magnific popup iframe
+	/* =========================================================================  */
+	$('.video').magnificPopup({
+		type: 'iframe',
+		iframe: {
+		  patterns: {
+			dailymotion: {
+			  index: 'dailymotion.com',
+			  id: function(url) {        
+				  var m = url.match(/^.+dailymotion.com\/(video|hub)\/([^_]+)[^#]*(#video=([^_&]+))?/);
+				  if (m !== null) {
+					  if(m[4] !== undefined) {
+						  return m[4];
+					  }
+					  return m[2];
+				  }
+				  return null;
+			  },
+			  src: 'https://www.dailymotion.com/embed/video/%id%'
+			}
+		  }
+		}
+	  });
 	/* ========================================================================= */
 	/*	Portfolio Filtering Hook
 	/* =========================================================================  */
