69612b2c805954235283e786d7561d715d1da2cf
jhbrunoKim <bobhappy2000@gmail.com>
2020-03-06 17:42:20 +0900

    youtube 수정.

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,20 +41,22 @@ jQuery(function ($) {
 		type: 'iframe',
 		iframe: {
 		  patterns: {
-			dailymotion: {
-			  index: 'dailymotion.com',
-			  id: function(url) {        
-				  var m = url.match(/^.+dailymotion.com\/(video|hub)\/([^_]+)[^#]*(#video=([^_&]+))?/);
-				  if (m !== null) {
-					  if(m[4] !== undefined) {
-						  return m[4];
-					  }
-					  return m[2];
-				  }
-				  return null;
+			youtube: {
+				index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+
+				id: function(url) {        
+                    var m = url.match(/[\\?\\&]v=([^\\?\\&]+)/);
+                    if ( !m || !m[1] ) return null;
+                    return m[1];
+                },
+		  
+				//id: 'v=', // String that splits URL in a two parts, second part should be %id%
+				// Or null - full URL will be returned
+				// Or a function that should return %id%, for example:
+				// id: function(url) { return 'parsed id'; }
+		  
+				src: '//www.youtube.com/embed/%id%?autoplay=1' // URL that will be set as a source for iframe.
 			  },
-			  src: 'https://www.dailymotion.com/embed/video/%id%'
-			}
 		  }
 		}
 	  });
